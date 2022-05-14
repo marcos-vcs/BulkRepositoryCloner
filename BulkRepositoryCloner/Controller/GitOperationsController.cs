221fbf62d85948eed6a87b731d67eb3d068413a7
marcos-vcs <marcosviniciocardoso123@gmail.com>
2022-05-14 11:07:09 -0300

    feature: update controller and view

--- BulkRepositoryCloner/Controller/GitOperationsController.cs
+++ BulkRepositoryCloner/Controller/GitOperationsController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace BulkRepositoryCloner.Controller
 {
     public class GitOperationsController
     {
-        public bool IsGitInstalled()
+
+        public static bool IsGitInstalled()
         {
             const string GitPath = @"C:\Program Files\Git";
 
@@ -26,5 +28,71 @@ namespace BulkRepositoryCloner.Controller
                 return false;
             }
         }
+
+        private static bool IsTxtCreated()
+        {
+            try
+            {
+                const string TxtName = "LIST_OF_REPOSITORY.txt";
+                string ApplicationPath = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+                return new FileInfo(ApplicationPath + TxtName).Exists;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public static bool CreateTxt()
+        {
+            const string TxtName = "LIST_OF_REPOSITORY.txt";
+            string ApplicationPath = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+
+            try
+            {
+                if (!IsTxtCreated())
+                {
+                    File.Create(Path.Combine(ApplicationPath, TxtName));
+                    return IsTxtCreated();
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public static void ExecuteGitCommands()
+        {
+            try
+            {
+                string GitCommand = "git";
+                string GitClone = "-C C:\\git clone ";
+                const string TxtName = "LIST_OF_REPOSITORY.txt";
+                string ApplicationPath = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+
+                using (StreamReader sr  = File.OpenText(ApplicationPath + TxtName))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Process.Start(GitCommand, GitClone + line);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
     }
 }
--- BulkRepositoryCloner/Views/View.cs
+++ BulkRepositoryCloner/Views/View.cs
@@ -10,11 +10,60 @@ namespace BulkRepositoryCloner.Views
             Console.WriteLine("---------------------------------------");
             Console.WriteLine("DOWNLOAD GIT REPOSITORY IN BULK");
             Console.WriteLine("---------------------------------------");
+            Requirements();
         }
 
-        public static void Requirements()
+        private static void Requirements()
         {
-            GitOperationsController.
+           Console.WriteLine("Checking if GIT is installed...");
+           bool result =  GitOperationsController.IsGitInstalled();
+
+            if (result)
+            {
+                Console.WriteLine("GIT PROPERLY INSTALLED.");
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("VERIFYING TEXT FILE LOCATED ALONG WITH THIS PROGRAM...");
+
+                if (GitOperationsController.CreateTxt())
+                {
+                    Console.WriteLine("A TEXT FILE NAMED LIST_OF_REPOSITORY.txt WAS CREATED WITH THIS PROGRAM.");
+                    Console.WriteLine("THERE WILL BE ALL THE LINKS OF ALL THE GIT REPOSITORY,\n ADD THE LINK TO THE REPOSITORY YOU WANT TO CLONE, EACH LINK IN A FILE LINE.");
+                    Exit();
+                }
+                else
+                {
+                    Console.WriteLine("ARCHIVE OK");
+                    Console.WriteLine("PRESS ENTER TO START CLONING: ");
+                    Console.ReadLine();
+                    GitClone();
+                }
+            }
+            else
+            {
+                Console.WriteLine("THE PREREQUISITE FOR THIS PROGRAM IS TO HAVE GIT INSTALLED,\n WE CAN'T FIND GIT. PLEASE INSTALL AND RUN AGAIN.");
+                Exit();
+            }
+
+        }
+
+        private static void Exit()
+        {
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("PRESS ENTER TO CLOSE");
+            Console.WriteLine("---------------------------------------");
+            Console.ReadLine();
+        }
+
+        private static void GitClone()
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(@"BY DEFAULT ALL CLONED REPOSITORY WILL BE AVAILABLE IN C:\GIT");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("STARTING THE REPOSITORY CLONING PROCESS...");
+            Console.WriteLine();
+            GitOperationsController.ExecuteGitCommands();
+
         }
     }
 }
